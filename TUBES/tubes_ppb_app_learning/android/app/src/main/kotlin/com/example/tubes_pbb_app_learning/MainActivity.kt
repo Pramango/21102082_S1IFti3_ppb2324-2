ef8082ba8d27efcf4082d40d3dfad2251f41bc82
Pramango <anandafikrisatriyohastomo@gmail.com>
2024-05-03 10:34:41 +0700

    login_firebase_auth selesai

---
+++ TUBES/tubes_ppb_app_learning/android/app/src/main/kotlin/com/example/tubes_pbb_app_learning/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tubes_pbb_app_learning
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ TUBES/tubes_ppb_app_learning/lib/styles/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const Color kBgColor = Color.fromARGB(255, 173, 185, 255);
+const Color kBlueRibbon = Color(0xFF195BDC);
+const Color kNightBlack = Color(0xFF2C2C2C);
+const Color kBlack = Color(0xFF000000);
+const Color kMatterhornBlack = Color(0xFF525252);
+const Color kNobelGray = Color(0xFF9F9F9F);
+const Color kLightGray = Color(0xFFC8C8C8);
+const Color kSuvaGray = Color(0xFF868686);
+const Color kGray = Color(0xFF888888);
+const Color kDarkGray = Color(0xFFABABAB);
+const Color kWhite = Color(0xFFFFFFFF);
