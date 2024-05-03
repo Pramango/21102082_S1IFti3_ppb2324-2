8b71630c96584a430f842dc8a8d04c88cfc4146f
Pramango <anandafikrisatriyohastomo@gmail.com>
2024-05-03 13:07:29 +0700

    login user telah selesai

--- praktikum_09/firebase_auth(Pertemuan9)/lib/main.dart
+++ praktikum_09/firebase_auth(Pertemuan9)/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pertemuan9/bloc/login/login_cubit.dart';
 import 'package:pertemuan9/ui/splash.dart';
 import 'package:pertemuan9/utils/routes.dart';
 import 'firebase_options.dart';
@@ -17,12 +19,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Praktikum 9",
-      debugShowCheckedModeBanner: false,
-      navigatorKey: NAV_KEY,
-      onGenerateRoute: generateRoute,
-      home: SplashScreen(),
+    return MultiBlocProvider(
+      providers: [BlocProvider(create: (context) => LoginCubit())],
+      child: MaterialApp(
+        title: "Praktikum 6",
+        debugShowCheckedModeBanner: false,
+        navigatorKey: NAV_KEY,
+        onGenerateRoute: generateRoute,
+        home: SplashScreen(),
+      ),
     );
   }
 }
